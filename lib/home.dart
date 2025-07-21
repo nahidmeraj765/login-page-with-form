a3908378f5413fc0fa42e29f27fa983295b970e8
NaHid Hasan Meraj <nahid.meraj765@gmail.com>
2025-07-21 17:54:32 +0600

    adding validator in final commit

--- lib/home.dart
+++ lib/home.dart
@@ -1,111 +1,152 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+
 class Home extends StatelessWidget {
-  const Home({super.key});
+  Home({super.key});
+
+  final _formKey = GlobalKey<FormState>();
+  TextEditingController phoneController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-       ),
+      appBar: AppBar(),
 
-       body: Padding(
-         padding: const EdgeInsets.all(20.0),
-         child: Form(
-          child: Column(
-            children: [
-              SizedBox(
-                height: 30,
-              ),
-              Image.network(
-                'https://i0.wp.com/socialkings.online/wp-content/uploads/2021/03/Artboard-1twitter.png?fit=650%2C534&ssl=1', width: 200),
-              SizedBox(
-                height: 40,
-              ),
-              Text("Log in to Twitter", style: GoogleFonts.nunito(
-                fontSize: 26,
-                fontWeight: FontWeight.bold,
-              ),
-              ),
-              SizedBox(
-                height: 40,
-              ),
-              TextFormField(
-                decoration: InputDecoration(
-                  prefixIcon: Icon(Icons.person_outline),
-                  labelText: "Phone, email or username",
-                  labelStyle: GoogleFonts.nunito(
-                    fontSize: 14,
-                    fontWeight: FontWeight.w400,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(40.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                SizedBox(height: 30),
+                Image.network(
+                  'https://i0.wp.com/socialkings.online/wp-content/uploads/2021/03/Artboard-1twitter.png?fit=650%2C534&ssl=1',
+                  width: 200,
+                ),
+                SizedBox(height: 40),
+                Text(
+                  "Log in to Twitter",
+                  style: GoogleFonts.nunito(
+                    fontSize: 30,
+                    fontWeight: FontWeight.w800,
                   ),
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(25),
-                    borderSide: BorderSide(
+                ),
+                SizedBox(height: 40),
+                TextFormField(
+                  controller: phoneController,
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.person_outline),
+                    labelText: "Phone, email or username",
+                    labelStyle: GoogleFonts.nunito(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w400,
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(25),
+                      borderSide: BorderSide(
+                        color: Color(0xFF1DA1F2),
+                        width: 1.5,
+                      ),
+                    ),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "No Field can be empty";
+                    } else if (value.length != 11) {
+                      return "Phone number must contain 11 digits";
+                    } else {
+                      return null;
+                    }
+                  },
+                ),
+                SizedBox(height: 25),
+                TextFormField(
+                  controller: passwordController,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.lock_outline),
+                    labelText: "Password",
+                    labelStyle: GoogleFonts.nunito(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w400,
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(25),
+                      borderSide: BorderSide(
+                        color: Color(0xFF1DA1F2),
+                        width: 1.5,
+                      ),
+                    ),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "No Field can be empty";
+                    } else if (value.length < 8) {
+                      return "Password must contain 8 characters";
+                    } else {
+                      return null;
+                    }
+                  },
+                ),
+                SizedBox(height: 30),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.radio_button_checked,
+                      size: 20,
                       color: Color(0xFF1DA1F2),
-                      width: 1.5,
+                    ),
+                    SizedBox(width: 6),
+                    Text(
+                      "Remember me",
+                      style: GoogleFonts.nunito(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 20),
+                SizedBox(
+                  height: 70,
+                  width: 300,
+                  child: OutlinedButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(content: Text("Log in Successful")),
+                        );
+                      }
+                    },
+                    style: OutlinedButton.styleFrom(
+                      backgroundColor: Color(0xFF1DA1F2),
+                    ),
+                    child: Text(
+                      "Log In",
+                      style: GoogleFonts.nunito(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.w500,
+                      ),
                     ),
                   ),
                 ),
-              ),
-              SizedBox(
-                height: 25,
-              ),
-              TextFormField(
-                decoration: InputDecoration(
-                  prefixIcon: Icon(Icons.lock_outline),
-                  labelText: "Password",
-                  labelStyle: GoogleFonts.nunito(
+                SizedBox(height: 20),
+                Text(
+                  "Don't have an account?",
+                  style: GoogleFonts.nunito(
                     fontSize: 14,
-                    fontWeight: FontWeight.w400,
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(25),
-                    borderSide: BorderSide(
-                      color: Color(0xFF1DA1F2),
-                      width: 1.5,
-                    ),
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              Text("Remember me", style: GoogleFonts.nunito(
-                fontSize: 14,
-                fontWeight: FontWeight.w500,
-              ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              SizedBox(
-                height: 70,
-                width: 300,
-                child: OutlinedButton(
-                  onPressed: (){},
-                  style: OutlinedButton.styleFrom(
-                  backgroundColor: Color(0xFF1DA1F2),
-                ),
-                  child: Text("Submit", style: GoogleFonts.nunito(
-                    color: Colors.white,
-                    fontSize: 18,
                     fontWeight: FontWeight.w500,
+                  ),
                 ),
-                ),
-                ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Text("Don't have an account?", style: GoogleFonts.nunito(
-                fontSize: 14,
-                fontWeight: FontWeight.w500,
-              ),
-              ),
-            ],
+              ],
+            ),
           ),
-         ),
-       ),
+        ),
+      ),
     );
   }
 }
